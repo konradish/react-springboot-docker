cd6604c2dbd9691eb29550bf6420570999bc82e5
konradish <konradish@gmail.com>
2020-02-09 14:59:31 -0600

    Update create_fe_be.cmd

--- create_fe_be.cmd
+++ create_fe_be.cmd
@@ -1,4 +1,4 @@
-md be
+mkdir be
 docker run -v C:\projects\react-springboot-docker\be:/app -it openjdk sh -c "cd /app && curl https://start.spring.io/starter.tgz -d dependencies=web,devtools | tar zxvf - && ./mvnw install"
-md fe
+mkdir fe
 docker run -v C:\projects\react-springboot-docker\fe:/app -it node sh -c "cd /app && npm install -g create-react-app && create-react-app ."
