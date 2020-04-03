fa4f1fd5c111372c2088ff785af3cfe9c3413ccf
Konrad <konradish@gmail.com>
2020-04-03 17:33:55 -0500

    Put in a risky chmod 777
    to avoid permission problems on the host

--- run-be.sh
+++ run-be.sh
@@ -2,6 +2,7 @@
 cd /app
 if [ ! -f ./mvnw ]; then
     curl https://start.spring.io/starter.tgz -d dependencies=web,devtools | tar zxvf - && \
+    chmod -R 777 .
     ./mvnw install
 fi
 ./mvnw spring-boot:run
